a1d4cf183f63dc12af8e692bafecf36986e84932
samuel.nathanson@gmail.com <samuel.nathanson@gmail.com>
2021-11-24 17:50:39 -0500

    Implementing value iteration algorithm

--- examples/ReinforcementLearning/Racetrack.py
+++ examples/ReinforcementLearning/Racetrack.py
@@ -5,7 +5,8 @@ import os
 import traceback
 import numpy as np
 import sys
-import matplotlib.pyplot as plt
+import random
+
 
 def racetrack_experiment(track_name: str,
                          track_matrix: np.chararray,
@@ -16,6 +17,116 @@ def racetrack_experiment(track_name: str,
                          convergence_threshold: float,
                          results_dict: dict,
                          lock):
+    '''
+    :param track_name:
+    :param track_matrix:
+    :param algorithm_name:
+    :param crash_scenario_name:
+    :param discount_factor:
+    :param learning_rate:
+    :param convergence_threshold:
+    :param results_dict:
+    :param lock:
+    :return:
+    '''
+
+
+    '''
+    ================================================================
+    Random Action Functions
+    ================================================================
+
+    '''
+    def get_random_acceleration():
+        return random.choice(get_possible_accelerations())
+
+
+    def get_random_direction():
+        return random.choice(get_possible_directions())
+
+
+    '''
+    ================================================================
+    State + Action Possibilities
+    ================================================================
+    '''
+
+    def update_state(state, action):
+        position = (state[0], state[1])
+        velocity = (state[2], state[3])
+
+        direction = (action[0], action[1])
+        acceleration_magnitude = (action[2])
+
+        if(random.random() > get_probability_of_action_success()):
+            safe_print("Action failed")
+        else:
+            new_v_y = velocity[0] + direction[0] * acceleration_magnitude
+            new_v_x = velocity[1] + direction[1] * acceleration_magnitude
+            velocity = (new_v_y, new_v_x)
+
+
+    def get_probability_of_action_success():
+        return 0.8
+
+    def get_possible_accelerations():
+        return [-1, 0, 1]
+
+
+    def get_possible_directions():
+        north = (1,0)
+        east = (0, 1)
+        south = (-1, 0)
+        west = (0, -1)
+        return [north, east, south, west]
+
+
+    def get_possible_velocities():
+        velocities = []
+        for v_x in range(-5,6):
+            for v_y in range(-5,6):
+                velocities.append((v_y, v_x))
+        return velocities
+
+
+    def get_possible_positions():
+        positions = []
+        for y, x in np.ndindex(track_matrix.shape):
+            positions.append((y,x))
+        return positions
+
+    def get_possible_valid_positions():
+        valid_positions = []
+        for (y,x) in get_possible_positions():
+            if(track_matrix[y,x] == '.'):
+                valid_positions.append((y,x))
+        return valid_positions
+
+
+    def get_possible_actions():
+        actions = []
+        for direction in get_possible_directions():
+            for acceleration in get_possible_accelerations():
+                actions.append((direction + acceleration))
+
+
+    def get_possible_valid_states():
+        valid_states = []
+        for position in get_possible_valid_positions():
+            for velocity in get_possible_velocities():
+                state = position + velocity
+                valid_states.append(state)
+        return valid_states
+
+
+    def get_possible_states():
+        states = []
+        for position in get_possible_positions():
+            for velocity in get_possible_velocities():
+                state = position + velocity
+                states.append(state)
+        return states
+
 
     def safe_print(*args):
         lock.acquire()
@@ -23,12 +134,62 @@ def racetrack_experiment(track_name: str,
         sys.stdout.flush()
         lock.release()
 
+
+
+    def reward(state, action):
+        new_state = update_state(state, action)
+
+    def value_iteration_experiment():
+        # initialize states
+        v = {}
+        q = {}
+        pi = {}
+        valid_states = get_possible_valid_states
+        actions = get_possible_actions()
+        for state in valid_states():
+            v[state] = random.random() * 10 - 5 # Arbitrary random values between -5, 5
+
+        t = 0
+        while True:
+            t = t+1
+            q[t] = {}
+            pi[t] = {}
+            v[t] = {}
+
+            for s in valid_states:
+                q[t][s] = {}
+                for a in actions:
+                    q[t][s][a] = reward(s,a) + discount_factor * sum([get_probability_of_action_success()])
+
+                pi[t][s] = max(q[t][s], key=q[t][s].get)
+                v[t][s] = q[t][s][pi[t][s]]
+
+            if(all(i < convergence_threshold for i in v[t][s])):
+                break
+
+
+    def q_learning_experiment():
+        pass
+
+    def sarsa_experiment():
+        pass
+
+    '''
+    Run experiment
+    '''
+    experiments = {
+        "value-iteration" : value_iteration_experiment,
+        "q-learning" : q_learning_experiment,
+        "SARSA": sarsa_experiment
+    }
+
     safe_print(f"Reporting from process: Experiment {algorithm_name} on matrix of shape {track_matrix.shape} with crash scenario {crash_scenario_name}")
 
-'''
+    result = experiments[algorithm_name]()
 
-    
-'''
+
+
+
 def read_racetrack_file(file_name: str, tracks_dict: dict):
     '''
     Example
@@ -52,7 +213,7 @@ def read_racetrack_file(file_name: str, tracks_dict: dict):
     try:
         with open(file_name, "r") as racetrack_file:
             n_rows, n_cols = [int(x) for x in (racetrack_file.readline()).split(",")]
-            track_matrix = np.chararray([n_rows, n_cols])
+            track_matrix = np.chararray([n_rows, n_cols], unicode=True)
 
             for row in range(0, n_rows):
                 line = racetrack_file.readline()
@@ -193,8 +354,6 @@ def main():
     t1 = time.time()
     print(f"Average time per experiment: {np.average(list(experiment_timing.values()))}")
     print(f"Time to perform {len(experiment_timing.keys())} experiments concurrently: {t1 - t0}")
-
-    plt.pause(100)
 
 
 if __name__ == "__main__":
